86619d3960c5f8f090a89099e94cd11b14e91c6d
MLAndreaRavasi <118526622+MLAndreaRavasi@users.noreply.github.com>
2024-02-05 18:01:00 +0100

    dev with errors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var app = express();
 var port = 3000;
 
 var url = 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/';
-var apikey = 'RGAPI-7470ec64-8a81-4245-8b58-3f2b06d27c90';
+var apikey = 'RGAPI-ab8173f0-43c2-4c60-a60c-689105e83f9b';
 var summonerName = '';
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,11 +21,12 @@ app.get('/', (req, res) => {
 
 app.post('/submit', (req, res) => {
     res.send("<h1>Your summoner name is:<br> submit ok <br>" + req.body.summonerName);
-    getUser(req.body.summonerName);
+    let data = getUser(req.body.summonerName);
+    getEntries(data.id);
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`);
 })
 
 async function getUser(summonerName) {
@@ -33,7 +34,23 @@ async function getUser(summonerName) {
         const response = await axios.get(url + summonerName + '?api_key=' + apikey);
         if (response.status === 200) {
             console.log(response.status);
-            console.log(response.data.summonerLevel);
+            console.log(response.data.summonerLevel); //Expected: 534
+            console.dir(response.data);
+            return response.data;
+        }
+        else console.log(response.status);
+    } catch (error) {
+        console.error('Request error');
+        return error;
+    }
+}
+
+async function getEntries(encryptedId) {
+    try {
+        const response = await axios.get(url + encryptedId + '?api_key=' + apikey);
+        if (response.status === 200) {
+            console.dir(response.data);
+            return response.data;
         }
         else console.log(response.status);
     } catch (error) {
